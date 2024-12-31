96fa857422caa53ffcf43c8701e8f13ee65d73d6
doomdybbuk <manan.jain16949@gmail.com>
2024-12-31 16:52:10 +0530

    `feat: added authentication and session management using passportjs and connect-mongodb-session`

--- NOTES.md
+++ NOTES.md
@@ -19,3 +19,19 @@
 # What are Resolvers?
 - Resolvers are functions that determine how to fetch the data and associated with each field in the schema.
 - They are responsible for resolving the queries and mutations defined in the type definitions and returning the requested data.
+
+# What is Serialization?
+- It is the process of converting the user objects into a format that can be stored and retrieved easily.
+
+# Authentication
+- We will be using passportjs for the authentication of the users.
+- First with the help of local auth strategy we will be either logging in or registering a user after that we will serialize the user object so that it can be stored as session (userId) in the database then it will be then used as (connet.sid) sid ie session id for next time logging in(verification).
+
+- We will be using local storage for authetication and it will be Http only which makes it more secure and it cannot be accessed by javaScript inorder to get the details   
+
+
+# "connect-mongodb-session": "^5.0.0",
+- This will allow us to store the sessions created of the user object for authentication.
+
+# "passport": "^0.7.0", "graphql-passport": "^0.6.8",
+- This the the main authetication library/ package that will be used. Since passport alone itself just provides compatibilty with restapi and since we are using graphql api we will be using graphql-passport which will make it compatible to work passport authetication with graphql.
--- backend/index.js
+++ backend/index.js
@@ -7,12 +7,44 @@ import mergedResolvers from "./resolvers/index.js";
 import mergedTypeDefs from "./typeDefs/index.js";
 import { expressMiddleware } from "@apollo/server/express4";
 import { ApolloServerPluginDrainHttpServer } from "@apollo/server/plugin/drainHttpServer";
+import {connectDB} from "./db/connectDB.js";
+import passport from "passport";
+import session from "express-session";
+import connectMongo from "connect-mongodb-session";
+
 
 dotenv.config();
 
 const app = express();
 
 const httpServer = http.createServer(app);
+
+const MongoDBStore = connectMongo(session);
+
+const store = new MongoDBStore({
+  uri : process.env.MONGO_URI,
+  collection: "sessions",
+});
+
+store.on("error",(err) => {
+  console.error(err);
+});
+
+app.use(
+  session({
+    secret : process.env.SESSION_SECRET,
+    resave: false, //This option specifies that weather to save the session to the store on every request or not. If this is saved as true then inside mongodb there will be different sessions for the same user.
+    saveUninitialized: false, // Option specifies that weather to save uninitialized session or not. A session is uninitialized when it is new but not modified.
+    cookie:{
+      maxAge: 1000*60*60*24*7, // 1 week
+      httpOnly: true, //This option specifies that the cookie will only be available for the server and not for the client side javascript.This makes the application more secure.It basically prevents cross-site scripting attacks.
+    },
+    store:store, //This is the store where the session will be stored.
+  })
+)
+
+app.use(passport.initialize()); //This is used to initialize the passport.
+app.use(passport.session()); //This is used to maintain the session.
 
 const server = new ApolloServer({
   typeDefs: mergedTypeDefs,
@@ -32,5 +64,5 @@ app.use(
 );
 
 await new Promise((resolve) => httpServer.listen({ port: 4000 }, resolve));
-
+await connectDB();
 console.log(`Server ready at http://localhost:4000`);
