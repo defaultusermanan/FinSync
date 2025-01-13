2c431e0f215e197270a8508f932c7ebef08e9734
doomdybbuk <manan.jain16949@gmail.com>
2025-01-13 17:37:41 +0530

    FrontEnd Files configuration. added the routing functionality and created the different pages.

---
+++ frontend/src/App.jsx
@@ -0,0 +1,17 @@
+import { Route, Routes } from "react-router-dom"
+
+function App() {
+  return (
+    <>
+      <Routes>
+        <Route path ="/" element={ <HomePage /> } />
+        <Route path ="/login" element={ <LoginPage /> } /> 
+        <Route path ="/signup" element={ <SignUpPage /> } />
+        <Route path ="/transaction/:id" element={ <TransactionPage /> } />
+        <Route path ="*" element={ <NotFound /> } />   
+      </Routes>
+    </>
+  )
+}
+
+export default App
